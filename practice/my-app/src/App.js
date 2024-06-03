60d9f74318b1c30a08065a3f247ad3be02dab32f
Ajit <ajitadsul1996@gmail.com>
2024-06-03 12:38:26 +0530

    add

--- practice/my-app/src/App.js
+++ practice/my-app/src/App.js
@@ -4,37 +4,48 @@ import { Axios } from 'axios';
 //import pract from "./Practice.js"
 import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
-
 function App(props) {
-  const [indexx,setIndexx]=useState(0)
-  const dataa=[{title:'abc',text:'Inside abc'},{title:'pqr',text:'Inside pqr'},{title:'jkl',text:'Inside jkl'},{title:'mno',text:'Inside mno'}]
-  const nextt=()=>{
-    if(indexx<dataa.length-1){
-      setIndexx(indexx+1)
-    }
-    else{
-      setIndexx(0)
-    }
+  var ele=false
+  const [idd,setId]=useState(0)
+  const [namee,setName]=useState('')
+  const [data,setData]=useState([{id:'',name:''}])
+  const abcc=(event)=>{
+    setId(event.target.value)
   }
-  const prevv=()=>{
-    if(indexx>0){
-      setIndexx(indexx-1)
-    }
-    else{
-      setIndexx(dataa.length-1)
-    }
+
+  const abcc2=(event)=>{
+    setName(event.target.value)  
+  }
+  const abcc3=()=>{
+        setData([...data,{id:idd,name:namee}])
+        console.log(data);
+  }
+  function deletee(j){
+    //Reflect.deleteProperty(data[j],['name'])
+    setData(data.filter((item,i)=>i!=j))
+    console.log("okhj",data);
   }
   return (
-    <>
     <div className="App">
-    <h1>Hello World.................</h1>
-      <h1>{dataa[indexx].title}</h1>
-      <p>{dataa[indexx].text}</p>
+     
+      <form>
+      <label htmlFor='aa'>Enter ID :&nbsp;</label>
+       <input type='text' id="aa" className="abc" name="name1"  onChange={abcc}></input>
+       <br/>
+       <label htmlFor='aa'>Enter Name :&nbsp;</label>
+       <input type='text' id="aa2" className="abc" onChange={abcc2}></input>
+       <br/>
+       <button type='button' id='add' onClick={abcc3}>Add</button>
+       </form>
+       <table>
+        <thead>
+          <tr><th>ID</th><th>Name</th><th>Button</th></tr>
+        </thead>
+        <tbody>
+          {data.map((e,i)=>{return (<tr key={e.id}><td>{e.id}</td><td>{e.name}</td>{e.id!=0 &&<td><button type='button' onClick={()=>deletee(i)}>Delete</button></td>}</tr>)})}
+        </tbody>
+       </table>
     </div>
-  <button type='button' onClick={prevv}>Prev</button>
-  
-  <button type='button' onClick={nextt}>Next</button>
-    </>
   );
 }
 
